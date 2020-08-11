d08dfa65fce095d4706cf690d915c086bf671970
K-Kumar-01 <59891164+K-Kumar-01@users.noreply.github.com>
2020-08-11 20:41:41 +0530

    implemted promise method

--- controllers/categories.js
+++ controllers/categories.js
@@ -1,11 +1,11 @@
 const Category = require('../models/category');
 
 exports.getAllCategories = async (req, res) => {
-	try {
-		let categories = await Category.find({});
-	} catch (error) {
-		return res.status(500).json({ message: 'Server error' });
-	}
+	// try {
+	// 	let categories = await Category.find({});
+	// } catch (error) {
+	// 	return res.status(500).json({ message: 'Server error' });
+	// }
 
 	if (!categories) {
 		return res.status(500).json({ message: 'Server error' });
@@ -14,7 +14,19 @@ exports.getAllCategories = async (req, res) => {
 		return res.status(404).json({ message: 'No categories found' });
 	}
 
-	return res.status(200).json({
-		categories,
+	Category.find({}).then((err, categories) => {
+		if (err) {
+			return res.status(500).json({ message: 'Server error' });
+		}
+		if (!categories) {
+			return res.status(500).json({ message: 'Server error' });
+		}
+		if (categories.length === 0) {
+			return res.status(404).json({ message: 'No categories found' });
+		}
+
+		return res.status(200).json({
+			categories,
+		});
 	});
 };
