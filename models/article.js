a9e4ccff15ec6fae3a24906c840df96bff03f53f
Your Name <you@example.com>
2020-08-17 20:38:49 +0530

    Article model modified

--- models/article.js
+++ models/article.js
@@ -2,16 +2,31 @@ var mongoose = require('mongoose');
 
 
 // things will need to be changed later
-var articleSchema = new mongoose.Schema({
-	title: {
-		type: String,
-		index: true,
+var articleSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			index: true,
+			required: true
+		},
+		body: {
+			type: String,
+			required: true
+		},
+		featuredPhoto: {
+			data: Buffer,
+			contentType: String,
+		},
+		category:{
+			type: String,
+			required:true,
+			index: true,
+		}
+
 	},
-	body: {},
-	featuredPhoto: {
-		data: Buffer,
-		contentType: String,
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model('Article', articleSchema);
