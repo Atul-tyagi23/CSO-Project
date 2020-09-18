e37f27f7201b2faf7641b13906fc326294a80ccb
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-09-18 16:02:00 +0530

    Added middleware create article
    Create article will require token in header

--- routes/article.js
+++ routes/article.js
@@ -1,23 +1,29 @@
-const express = require('express');
-const { createArticle, allArticles } = require('../controllers/article');
-const multer = require('multer');
+const express = require("express");
+const { createArticle, allArticles } = require("../controllers/article");
+const multer = require("multer");
+const middlewareObj = require("../middleware/middleware");
 
 const router = express.Router();
 const storage = multer.diskStorage({
-	filename: function (req, file, callback) {
-		callback(null, Date.now() + file.originalname);
-	},
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  },
 });
 const imageFilter = function (req, file, cb) {
-	// accept image files only
-	if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-		return cb(new Error('Only image files are allowed!'), false);
-	}
-	cb(null, true);
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
 };
 const upload = multer({ storage: storage, fileFilter: imageFilter });
 
-router.get('/', allArticles);
-router.post('/create', upload.single('image'), createArticle);
+router.get("/", allArticles);
+router.post(
+  "/create",
+  upload.single("image"),
+  middlewareObj.extractAuthToken,
+  createArticle
+);
 
 module.exports = router;
