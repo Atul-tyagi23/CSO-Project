e2b40484f3d9632922f995f9778f7d0a75db54f5
K-Kumar01 <kushalkumargupta4@gmail.com>
2020-10-04 14:51:06 +0530

    Renamed the model file
    requestedArticle seemed confusing
    changes to requests

---
+++ models/requests.js
@@ -0,0 +1,38 @@
+var mongoose = require("mongoose");
+
+var requestSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      index: true,
+      required: true,
+    },
+    impPoints: {
+      type: String,
+      index: true,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    article: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Article",
+    },
+    postedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    closedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Request", requestSchema);
